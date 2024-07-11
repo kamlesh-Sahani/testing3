4ee7986fb61ba55b43ace632ae77322baa035f74
kamlesh_sahani <alex20150001969@gmail.com>
2024-07-11 16:38:07 +0530

    test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
-import './App.css'
+import './App.css';
 import LandingPage from './components/LandingPage';
 import Ask from './components/Ask';
-import Upload from './components/Upload'
-import { Routes,Route } from 'react-router-dom';
+import Upload from './components/Upload';
+import { Routes, Route } from 'react-router-dom';
 import TeacherRegister from './components/TeacherRegister';
 import StudentRegister from './components/StudentRegister';
 import { BouncyCardsFeatures } from './components/AboutUs';
@@ -35,53 +35,50 @@ import StudentCart from './components/StudentCart';
 import Layout from './components/Layout';
 import Success from './components/Success';
 import Cancel from './components/Cancel';
-import('preline')
+
+import('preline');
+
 function App() {
   return (
-  
     <Routes>
       <Route path='/' element={<LandingPage />} />
-      <Route path='/ask' element={<Ask/>}/>
-      <Route path='/upload' element={<Upload/>}/>
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/studentLogin' element={<StudentLogin/>}/>
-      <Route path='/teacherRegister' element={<TeacherRegister/>}/>
-      <Route path='/studentRegister' element={<StudentRegister/>}/>
-      <Route path='/about' element={<BouncyCardsFeatures/>}/>
-      <Route path='/navbar' element={<NavbarMain/>}/>
-      <Route path='/Modal' element={<Modal/>}/>
-      <Route path='/teacherNavbar' element={<TeacherNavBar/>}/>
-      <Route path='/studentNavbar' element={<StudentNavBar/>}/>
-      <Route path='/teacherProfile' element={<TeacherProfile/>}/>
-      <Route path='/createCourse' element={<CreateCourse/>}/>
-      <Route path='/test' element={<Test/>}/>
-      <Route path='/teacherMain' element={<TeacherMain/>}/>
-      <Route path='/studentMain' element={<StudentMain/>}/>
-      <Route path='/displayCourse' element={<DisplayCourse/>}/>
-      <Route path='/courseForm' element={<CourseForm/>}/>
-      <Route path='/singleCourse' element={<SingleCourse/>}/>
-      <Route path='/new' element={<NewTest/>}/>
-      <Route path='/courseForm' element={<CourseForm/>}/>
-      <Route path='/courseForm/:courseId' element={<CourseForm/>}/>
-      <Route path='/addChapter/:courseId' element={<AddChapter/>}/>
-      <Route path='/notification' element={<SlideInNotifications/>}/>
-      <Route path='/google' element={<GoogleAuth/>}/>
-      <Route path='/courseOverview/:courseId' element={<CourseOverview/>}/>
-      <Route path='*' element={<ErrorNotFound/>}/>
-      <Route path='/contact' element={<ContactUs/>}/>
-      <Route path='/studentProfile' element={<StudentProfile/>}/>
-      <Route path='/mycourses/:id' element={<MyCourses/>}/>
-      <Route path='/check' element={<Check/>}/>
-      <Route path='/search/:courseName' element={<SearchCourse/>}/>
-      <Route path='/cart/:studentId' element={<StudentCart/>}/>
-      {/* <Route path='/layout' element={<Layout/>}/> */}
-      {/* <Route path='/layout/:courseId' element={<Layout/>}/> */}
-      <Route path='/layout/:courseId/:chapter' element={<Layout/>}/>
-      <Route path='/success' element={<Success/>}/>
-      <Route path='/cancel' element={<Cancel/>}/>
+      <Route path='/ask' element={<Ask />} />
+      <Route path='/upload' element={<Upload />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/studentLogin' element={<StudentLogin />} />
+      <Route path='/teacherRegister' element={<TeacherRegister />} />
+      <Route path='/studentRegister' element={<StudentRegister />} />
+      <Route path='/about' element={<BouncyCardsFeatures />} />
+      <Route path='/navbar' element={<NavbarMain />} />
+      <Route path='/modal' element={<Modal />} />
+      <Route path='/teacherNavbar' element={<TeacherNavBar />} />
+      <Route path='/studentNavbar' element={<StudentNavBar />} />
+      <Route path='/teacherProfile' element={<TeacherProfile />} />
+      <Route path='/createCourse' element={<CreateCourse />} />
+      <Route path='/test' element={<Test />} />
+      <Route path='/teacherMain' element={<TeacherMain />} />
+      <Route path='/studentMain' element={<StudentMain />} />
+      <Route path='/displayCourse' element={<DisplayCourse />} />
+      <Route path='/courseForm' element={<CourseForm />} />
+      <Route path='/courseForm/:courseId' element={<CourseForm />} />
+      <Route path='/singleCourse' element={<SingleCourse />} />
+      <Route path='/new' element={<NewTest />} />
+      <Route path='/addChapter/:courseId' element={<AddChapter />} />
+      <Route path='/notification' element={<SlideInNotifications />} />
+      <Route path='/google' element={<GoogleAuth />} />
+      <Route path='/courseOverview/:courseId' element={<CourseOverview />} />
+      <Route path='*' element={<ErrorNotFound />} />
+      <Route path='/contact' element={<ContactUs />} />
+      <Route path='/studentProfile' element={<StudentProfile />} />
+      <Route path='/mycourses/:id' element={<MyCourses />} />
+      <Route path='/check' element={<Check />} />
+      <Route path='/search/:courseName' element={<SearchCourse />} />
+      <Route path='/cart/:studentId' element={<StudentCart />} />
+      <Route path='/layout/:courseId/:chapter' element={<Layout />} />
+      <Route path='/success' element={<Success />} />
+      <Route path='/cancel' element={<Cancel />} />
     </Routes>
-
-  )
+  );
 }
 
-export default App
+export default App;
